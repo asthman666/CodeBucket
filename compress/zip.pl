17cb091844402dedd93f0a111275dee915d5b026
asthman <4king-gruuby@163.com>
2016-12-04 14:37:19 +0800

    if output dir have the same name for zip, don't zip again

--- compress/zip.pl
+++ compress/zip.pl
@@ -3,29 +3,49 @@ use strict;
 use warnings;
 use Path::Tiny;
 use Getopt::Long;
+use Data::Dumper;
 
-my ($input_dir, $output_dir, $password);
+my ($input_dir, $zip_dir, $output_dir, $password, $help, $execute);
 GetOptions( "input_dir=s" => \$input_dir,
+	    "zip_dir=s" => \$zip_dir,   # the dir need to be ziped, default all dirs below input_dir 
 	    "output_dir=s" => \$output_dir,
 	    "password=s" => \$password,
+	    "help|?" => sub { usage() },
+	    "execute!" => \$execute,
     );
 
-
-if ( !$input_dir ) {
+if ( !$input_dir || !$password ) {
     usage();
-    exit 0;
 }
 
-my $dir = path($input_dir);
-foreach my $child ( $dir->children() ) {
+my $idir = path($input_dir);
+
+my @zip_files;
+
+if ( $output_dir ) {
+    my $odir = path($output_dir);
+    foreach ( $odir->children(qr/\.zip/) ) {
+	push @zip_files, $_->basename;
+    }
+}
+
+foreach my $child ( $idir->children() ) {
     if ( $child->is_dir ) {
 	my $dir_name = $child->basename;
 	next if $dir_name =~ /^\./;
-	
+	next if $zip_dir && $zip_dir ne $dir_name;
+	next if ( grep {"${dir_name}.zip" eq $_} @zip_files );
+
 	chdir($input_dir);
 
-	my $cmd = "/usr/bin/zip -r -P $password $dir_name.zip $dir_name/\n";
-	#print $cmd, "\n";
-	system($cmd);
+	$output_dir =~ s{/$}{};
+	my $cmd = "/usr/bin/zip -r -P $password '${output_dir}/${dir_name}.zip' '$dir_name'/";
+	print $cmd, "\n";
+	system($cmd) if $execute;
     }
+}
+
+sub usage {
+    print "Usage: perl zip.pl --output_dir /media/asthman/我的移动硬盘/photo --input_dir /media/asthman/000F7AEF000C7375/photo --password xxx\n";
+    exit 0;
 }
