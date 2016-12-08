ec664880ffed571e9809ee30336ae62242c91833
asthman <4king-gruuby@163.com>
2016-12-08 14:42:20 +0800

    fix simple

--- webdriver/chrome.pl
+++ webdriver/chrome.pl
@@ -6,7 +6,9 @@ use Selenium::Chrome;
 my $driver = Selenium::Chrome->new(proxy => {proxyType => 'direct'});
 my $url = "http://www.baidu.com";
 $driver->get($url);
-sleep 10;
+my $current_url = $driver->get_current_url;
+print "current_url: $current_url", "\n";
+sleep 5;
 $driver->shutdown_binary;
 
 
