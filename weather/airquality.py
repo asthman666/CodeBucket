58bae410148e75139a9c66d47c56ba6f4152c00b
宋晓楠 <4king-gruuby@163.com>
2019-02-06 22:45:33 +0800

    add air quality fetch

---
+++ weather/airquality.py
@@ -0,0 +1,60 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import re
+import datetime
+import os
+import config
+import pytz
+
+class AirQuality(object):
+    def extract_data(self, url):
+        headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+                    'Referer': 'https://weather.com/',
+                    'Upgrade-Insecure-Requests': '1'
+        }
+        f = requests.get(url, headers=headers)
+        web_content = f.text        
+        with open('web_file/{}-{}.txt'.format("airquality", datetime.date.today().isoformat()), 'w', encoding='utf8') as file:
+            file.write(web_content)
+        
+        soup = BeautifulSoup(web_content, 'lxml')
+        datas = {"column_headers": ['name', 'unit', 'num', 'desc'],
+                 "column_datas": []
+        }
+
+        names = []
+        units = []
+        descs = []
+        nums = []
+
+        for name in soup.select('div[class^="styles__rowPollutantName__"]'):
+            names.append(name.get_text())
+            units.append(name.next_sibling.get_text())
+            #print(name.get_text())
+            #print(name.next_sibling.get_text())
+        
+        for desc in soup.select('div[class^="styles__colPollutantLevel__"]'):
+            descs.append(desc.get_text())
+            #print(desc.get_text())
+
+        for num in soup.select('div[class^="styles__primaryPollutantGraphNumber__"]'):
+            nums.append(num.get_text())
+            #print(num.get_text())
+
+        if len(names) == len(descs) and len(names) == len(nums):
+            for i in range(0, len(names)):
+                primary = 0
+                if i == 0:
+                    primary = 1
+                datas['column_datas'].append([names[i], units[i], nums[i], descs[i], primary])
+        
+        return datas
+
+if __name__ == '__main__':
+    url = 'https://weather.com/zh-CN/forecast/air-quality/l/CHXX0141:1:CH'
+    airq = AirQuality()
+    datas = airq.extract_data(url)
+    print(datas)
+    
--- weather/kicktodb.py
+++ weather/kicktodb.py
@@ -5,6 +5,8 @@ import pymysql.cursors
 import config
 import datetime
 from weather import Weather
+from airquality import AirQuality
+import pytz
 
 def save2db(datas):
     dt_fetch = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -27,9 +29,37 @@ def save2db(datas):
     finally:
         connection.close()
 
+def save_air_quality2db(datas):
+    dt_fetch = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    tz = pytz.timezone('Asia/Shanghai')
+    dt = (datetime.datetime.now(tz).date()).isoformat()
+
+    # Connect to the database
+    connection = pymysql.connect(host=config.db['host'],
+                                user=config.db['user'],
+                                password=config.db['password'],
+                                db=config.db['db'],
+                                charset=config.db['charset'],
+                                cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            for d in datas['column_datas']:
+                sql = "INSERT INTO `airquality` (`dt_fetch`, `dt`, `name`, `unit`, `num`, `description`) VALUES (%s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (dt_fetch, dt, d[0], d[1], d[2], d[3]))
+        connection.commit()
+    finally:
+        connection.close()
+
 if __name__ == '__main__':
     url = 'https://weather.com/weather/5day/l/CHXX0141:1:CH'
     weather = Weather()
     datas = weather.get_datas(url)
+
+    url = 'https://weather.com/zh-CN/forecast/air-quality/l/CHXX0141:1:CH'
+    airq = AirQuality()
+    airq_datas = airq.extract_data(url)
+
     #print(datas)    
     save2db(datas)
+    save_air_quality2db(airq_datas)
